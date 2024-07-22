f3fe45fe80d8dfa31ec834214ef6e030deae91f9
mmayyiisuay <natchasuaysaard@gmail.com>
2024-07-22 19:21:23 +0700

    stucture u-u

--- src/app/view/layout.tsx
+++ src/app/view/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${mitr.variable} ${rubik.variable} ${baiJamjuree.variable}`}>
-      <body className={inter.className}>{children}</body>
+    <body className={inter.className}>{children}</body>
     </html>
-  );
-}
+    );
+  }
